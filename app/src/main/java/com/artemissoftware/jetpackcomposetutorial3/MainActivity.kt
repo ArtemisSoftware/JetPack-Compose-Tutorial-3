956de9da2e2fad38039ce9e847912ae7ead55825
gustavo <gustavo.isaac@gmail.com>
2021-11-25 08:09:08 +0000

    Create a circle card

--- app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/MainActivity.kt
@@ -3,11 +3,18 @@ package com.artemissoftware.jetpackcomposetutorial3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.artemissoftware.jetpackcomposetutorial3.ui.theme.JetPackComposeTutorial3Theme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +22,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetPackComposeTutorial3Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        color = Color(0xFF546E7A)
+    ) {
+        Text(text = "Hello ")
+    }
+
+
 }
+
+@Preview(showBackground = true)
+@Composable
+fun CreateCircle() {
+
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(45.dp),
+        shape = CircleShape
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetPackComposeTutorial3Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
