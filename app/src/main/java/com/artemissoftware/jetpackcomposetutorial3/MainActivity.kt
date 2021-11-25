c38f002d0fd594637a2fa169b788f41858d13c65
gustavo <gustavo.isaac@gmail.com>
2021-11-25 08:27:14 +0000

    MutableState - Incrementing a Counter

--- app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -64,12 +63,15 @@ fun MyApp() {
 @Composable
 fun CreateCircle() {
 
+
+    var moneyConter by remember{ mutableStateOf(0) }
+
     Card(
         modifier = Modifier
             .padding(3.dp)
             .size(105.dp)
             .clickable {
-
+                moneyConter += 1
             }
         ,
         shape = CircleShape,
