ac1710d88a0bb1f1032b239c7339fc8ce3aaa4f0
gustavo <gustavo.isaac@gmail.com>
2021-12-07 08:16:54 +0000

    Setting up the Navigation Structure

---
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/tothemovies/navigation/MovieNavigation.kt
@@ -0,0 +1,22 @@
+package com.artemissoftware.jetpackcomposetutorial3.ui.tothemovies.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MovieNavigation(){
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = MovieScreens.HomeScreen.name ){
+
+        composable(route = MovieScreens.HomeScreen.name){
+
+        }
+
+
+    }
+
+}
