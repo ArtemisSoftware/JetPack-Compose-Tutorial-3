38338966d2665b0ad6efe9a091fc46df06fb5254
gustavo <gustavo.isaac@gmail.com>
2021-12-10 19:13:34 +0000

    Using Coil to Add Images

--- app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/tothemovies/widgets/MovieRow.kt
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/tothemovies/widgets/MovieRow.kt
@@ -1,21 +1,23 @@
 package com.artemissoftware.jetpackcomposetutorial3.ui.tothemovies.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.artemissoftware.jetpackcomposetutorial3.ui.tothemovies.models.Movie
+import com.artemissoftware.jetpackcomposetutorial3.ui.tothemovies.models.getMovies
 
 @Composable
 fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}){
@@ -39,10 +41,31 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}){
                 shape = RectangleShape,
                 elevation = 4.dp
             ){
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+
+                Image(
+                    painter = rememberImagePainter(
+                        data = movie.images[0],
+                        builder = {
+                            crossfade(true)
+                            transformations(CircleCropTransformation())
+                        }),
+                    contentDescription = "Movie Poster")
             }
 
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(text = movie.title, style = MaterialTheme.typography.h6)
+                Text(text = "Director: ${movie.title}", style = MaterialTheme.typography.caption)
+                Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.caption)
+            }
+
         }
     }
+}
+
+@Preview
+@Composable
+private fun Preview(){
+
+    MovieRow(movie = getMovies()[0])
+    
 }
