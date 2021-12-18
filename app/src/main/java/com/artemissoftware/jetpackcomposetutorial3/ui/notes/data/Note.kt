503ced56e1f06265fede5bd3aa8d40fc201115be
gustavo <gustavo.isaac@gmail.com>
2021-12-18 16:35:46 +0000

    Creating a Note Entity

--- app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/notes/data/Note.kt
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/notes/data/Note.kt
@@ -1,12 +1,25 @@
 package com.artemissoftware.jetpackcomposetutorial3.ui.notes.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.time.LocalDateTime
 import java.util.*
 
+@Entity(tableName = "notes")
 class Note (
+
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name ="title")
     val title: String,
+
+    @ColumnInfo(name ="description")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+
+    @ColumnInfo(name ="entryDate")
+    val entryDate: Date = Date.from(Instant.now())
 
 )
