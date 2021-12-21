658e744beb753bb01180f6a23f77d241e7442127
gustavo <gustavo.isaac@gmail.com>
2021-12-21 08:32:09 +0000

    Finishing up the Repository Class

--- app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/jettrivia/repository/QuestionRepository.kt
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/jettrivia/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package com.artemissoftware.jetpackcomposetutorial3.ui.jettrivia.repository
 
+import android.util.Log
 import com.artemissoftware.jetpackcomposetutorial3.ui.jettrivia.data.DataOrException
 import com.artemissoftware.jetpackcomposetutorial3.ui.jettrivia.model.QuestionItem
 import com.artemissoftware.jetpackcomposetutorial3.ui.jettrivia.network.QuestionApi
@@ -11,4 +12,20 @@ class QuestionRepository @Inject constructor(private val api: QuestionApi) {
         DataOrException<ArrayList<QuestionItem>,
                 Boolean,
                 Exception>()
+
+
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, java.lang.Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+
+        }catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exc", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+
+        }
+        return dataOrException
+    }
 }
