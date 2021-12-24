848ffc158f45fe581d69eb95df1bf5dbd3cdb1d5
gustavo <gustavo.isaac@gmail.com>
2021-12-24 16:32:43 +0000

    Creating UI - Dotted Line Composable

--- app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/jettrivia/component/Questions.kt
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial3/ui/jettrivia/component/Questions.kt
@@ -1,5 +1,6 @@
 package com.artemissoftware.jetpackcomposetutorial3.ui.jettrivia.component
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
@@ -10,6 +11,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -71,6 +74,7 @@ fun QuestionDisplay(
 
 ) {
 
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
 
     Surface(
         modifier = Modifier
@@ -87,25 +91,27 @@ fun QuestionDisplay(
         ) {
 
             QuestionTracker()
+            DrawDottedLine(pathEffect = pathEffect)
         }
     }
 }
 
-//@Composable
-//fun DrawDottedLine(pathEffect: PathEffect) {
-//    androidx.compose.foundation.Canvas(modifier = Modifier
-//        .fillMaxWidth()
-//        .height(1.dp),
-//    ){
-//        drawLine(color = AppColors.mLightGray,
-//            start = Offset(0f, 0f),
-//            end = Offset(size.width, y = 0f),
-//            pathEffect = pathEffect)
-//
-//    }
-//
-//}
-//
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp),
+    ){
+        drawLine(color = AppColors.mLightGray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, y = 0f),
+            pathEffect = pathEffect)
+
+    }
+
+}
+
 //@Preview
 //@Composable
 //fun ShowProgress(score: Int = 12) {
